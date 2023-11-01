9a5018d5a4355e11f49189bf1d71f6d40254c6c5
shubham-purohit <shubhampurohit24@hotmail.com>
2023-11-02 01:31:02 +0530

    local storage added in todo app

--- todo_app/script.js
+++ todo_app/script.js
@@ -1,4 +1,3 @@
-toDoList = []
 let initialId = 1;
 let pending_task_count = 0;
 let completed_task_count = 0;
@@ -7,85 +6,113 @@ function Note(text) {
     this.noteId = initialId++;
     this.text = text;
     this.completed = false;
-    this.dateClosed = null;
+    this.isDeleted = false;
+}
+
+const getNoteListFromStorage = () => {
+    const data = localStorage.getItem('toDoList');
+    return data ? JSON.parse(data) : []
+}
+
+const deleteNote = (noteId) => {
+    const data = localStorage.getItem('toDoList');
+    const toDoList = data ? JSON.parse(data) : []
+    toDoList.forEach(item => {
+        if(item.noteId===noteId) {
+            if(item.completed) {
+                item.isDeleted = true;
+            } else {
+                const flag = confirm("Task is not yet complete. Proceed to delete?");
+                item.isDeleted = flag ? true : false;
+            }
+        }
+    });
+    localStorage.setItem('toDoList', JSON.stringify(toDoList))
+    populateNoteList(toDoList);
 }
 
 const updateNote = (noteId) => {
-    console.info(`Note id is ${noteId}`);
+    const data = localStorage.getItem('toDoList');
+    const toDoList = data ? JSON.parse(data) : []
     toDoList.forEach(item => {
         if(item.noteId==noteId) {
-            console.info('note found!!');
             if(item.completed) {
                 const flag = confirm("Do you want to mark it as incomplete?");
-                item.completed = flag?false:true;
+                item.completed = flag ? false : true;
             } else {
                 item.completed = true;
             }
         }
     });
-    populateNoteList();
+    localStorage.setItem('toDoList', JSON.stringify(toDoList))
+    populateNoteList(toDoList);
 };
 
-const createHTMLElementById = (element, id, onclickHanlder) => {
-
+const createListElement = (toDoObject) => {
+    let liElement = document.createElement('li');
+    let deleteButton = document.createElement('i');
+    let textelement = document.createElement('p');
+    deleteButton.className = 'fa-solid fa-xmark'
+    deleteButton.style = 'color: #ff2600'   
+    deleteButton.onclick = function() {
+        deleteNote(toDoObject.noteId);
+    }
+    textelement.innerText = toDoObject.text;
+    textelement.className = toDoObject.completed ? 'strike' : '';
+    liElement.id = toDoObject.noteId;
+    liElement.appendChild(textelement)
+    liElement.appendChild(deleteButton)
+    liElement.ondblclick = function() {
+        updateNote(toDoObject.noteId);
+    }
+    return liElement;
 }
 
-const populateNoteList = () => {
-    let ulPending = document.getElementById('pendingTask');
-    let ulCompleted = document.getElementById('completedTask');
+const populateNoteList = (toDoList) => {
+    note.value = '';
     pending_task_count = 0;
     completed_task_count = 0;
-    ulPending.innerHTML = '';
-    ulCompleted.innerHTML = '';
+    pendingTask.innerHTML = '';
+    completedTask.innerHTML = '';
     for(let i=0; i < toDoList.length; i++) {
-        let liElement = document.createElement('li');
-        let noteTile = document.createElement('button');
-        noteTile.innerText = toDoList[i].text;
-        noteTile.id = toDoList[i].noteId;
-        noteTile.onclick = function() {
-            updateNote(toDoList[i].noteId);
+        if(toDoList[i].isDeleted) {
+            console.log("skipped: " + toDoList[i].id);
+            continue;
         }
-        liElement.appendChild(noteTile);
-            
+        console.log("not skipped: " + toDoList[i].id);
+        const liElement = createListElement(toDoList[i])    
         if (toDoList[i].completed) {
-            noteTile.className = 'strike';
             completed_task_count++;
-            ulCompleted.appendChild(liElement);
+            completedTask.appendChild(liElement);
         } else {
             pending_task_count++;
-            ulPending.appendChild(liElement);
+            pendingTask.appendChild(liElement);
         }
     }
-    console.log(`${pending_task_count} pending tasks...`);
-    console.log(`${completed_task_count} completed tasks...`);
-    if(pending_task_count>0) {
-        document.getElementById('error-pending').style.display = "none";
-        console.log("here");
-    } else {
-        document.getElementById('error-pending').style.display = "block";
-    }
-    if(completed_task_count>0) {
-        document.getElementById('error-completed').style.display = "none";
-        console.log("THere");
-    } else {
-        document.getElementById('error-completed').style.display = "block";
-    }
+    error_pending.style.display = pending_task_count>0 ? "none" : "block";
+    error_completed.style.display = completed_task_count>0 ? "none" : "block";
 };
 
-function saveNote(btn_id) {
-    console.info(`This is button id: ${btn_id}`);
-    let noteText = document.getElementById("note").value;
+const saveNote = () => {
+    const noteText = note.value;
     if(noteText==undefined || noteText==null || noteText=='') {
         alert("Please insert valid value..");
         return;
     }
-    let newNote = new Note(noteText);
+    const newNote = new Note(noteText);
+    const toDoList = getNoteListFromStorage()
     toDoList.push(newNote);
-    reset();
-    console.log(toDoList);
-    populateNoteList();
+    localStorage.setItem('toDoList', JSON.stringify(toDoList))
+    populateNoteList(toDoList);
 }
 
-function reset() {
-    document.getElementById("note").value = '';
+const resetAll = () => {
+    const flag = confirm("Do you want to clear all tasks?");
+    if(!flag) {
+        return
+    }
+    localStorage.clear();
+    populateNoteList([]);
 }
+
+populateNoteList(getNoteListFromStorage())
