e9fd4a3b1d6fc8c24af5787fb02a5408b06e04d4
shubham-purohit <shubhampurohit24@hotmail.com>
2023-11-11 16:59:05 +0530

    minor UI enhancement for bill-splitter

--- bill-splitter/script.js
+++ bill-splitter/script.js
@@ -3,8 +3,19 @@ var bill = 0
 var selected_tip_id = ''
 var previous_tip_id = ''
 
+const tip_id_map = {
+    5 : "tip_5",
+    10 : "tip_10",
+    15 : "tip_15",
+    25 : "tip_25",
+    50 : "tip_50"
+}
+
+const toggle_outline = (id) => {
+    getElement(id + "_label").classList.toggle("input-outline-focus")
+}
+
 const input_value = (event, id) => {
-    console.log(event)
     var inputValue = event.target.value;
     if (!/^\d*\.?\d*$/.test(inputValue)) {
         // If not numeric, prevent the input
@@ -12,7 +23,8 @@ const input_value = (event, id) => {
         return;
     }
     if (id === "tip_custom") {
-        add_tip(tip_custom.value, id)
+        const tip_curr_id = tip_id_map[parseFloat(tip_custom.value)]
+        add_tip(tip_custom.value, tip_curr_id ?? id)
     } else {
         calculate_amount()
     }
@@ -36,7 +48,7 @@ const add_tip = (tip_percent, id) => {
 }
 
 const calculate_amount = () => {
-    selected_tip_id !== '' ? getElement(selected_tip_id).classList.add("tip-active") : tip = 0
+    selected_tip_id !== '' ? getElement(selected_tip_id).classList.add("tip-active") : tip = ''
     if (previous_tip_id !== '' && previous_tip_id !== selected_tip_id) {
         getElement(previous_tip_id).classList.remove("tip-active")
     }
@@ -52,14 +64,14 @@ const calculate_amount = () => {
     if (tip_not_present || count_not_present || bill_not_present) {
         tip_pp.innerText = "0.00"
     }
-    else
+    else   
         tip_pp.innerText = ((inputValue * tip / 100) / count).toFixed(2);
 
     if (count_not_present) {
-        getElement("bill_count").classList.add("input-red");
+        bill_count_label.classList.add("input-red");
         error_count.classList.add("error")
     } else {
-        bill_count.classList.remove("input-red");
+        bill_count_label.classList.remove("input-red");
         error_count.classList.remove("error")
     }
     if (input_not_present) {
@@ -76,7 +88,7 @@ const reset_data = () => {
     tip_pp.innerText = '0.00'
     total_pp.innerText = '0.00'
     selected_tip_id !== '' && getElement(selected_tip_id).classList.remove("tip-active")
-    bill_count.classList.remove("input-red")
+    bill_count_label.classList.remove("input-red")
     error_count.classList.remove("error")
     tip_custom.value = ''
     selected_tip_id = ''
